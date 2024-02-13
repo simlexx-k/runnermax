8c28ef45893c848f6b4d1657c0378aba43382bcf
simlexx-k <simonwriter254@gmail.com>
2024-02-13 16:33:40 +0300

    You are hereby commited!

---
+++ app/Livewire/PickPointTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PickPointTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.pick-point-table');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,10 +15,16 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <livewire:welcome.navigation />
-            @endif
+            <div>
+                @if (Route::has('login'))
+                    <livewire:welcome.navigation />
+                @endif
+                    <livewire:users-table />
+            </div>
+            <div>
                 <livewire:users-table />
+            </div>
+
         </div>
     </body>
 </html>
